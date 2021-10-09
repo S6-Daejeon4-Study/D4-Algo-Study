1f968dca1140937b342eed825bbf1fb0b1d3f5c5
zbqlr456@naver.com <zbqlr456@naver.com>
2021-10-09 16:53:01 +0900

    김응철

---
+++ D4S10/1주차/복습 문제/B10026_적록색약_김응철.java
@@ -0,0 +1,13 @@
+package MST;
+
+import java.io.*;
+import java.util.*;
+
+public class 적록색약_10026 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		
+		
+	}
+}
